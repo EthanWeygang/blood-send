13ac7ab2131e8c7679dbbe30eda447d84da0a2ff
Ethan Weygang <ethanweygang@gmail.com>
2024-11-11 02:49:06 +0000

    Added sign-up functionality and postcode to coordinates method

--- routes/signup-routes.js
+++ routes/signup-routes.js
@@ -1,3 +1,4 @@
+const axios = require("axios")
 const express = require("express")
 const session = require("express-session")
 const app = express()
@@ -17,7 +18,31 @@ router.get("/", async (req, res) => {
 })
 
 router.post("/", async(req, res) => {
+    
+    try{
+    const { firstname, lastname, email, password, phone, birthday, donor, postcode, bloodtype } = req.body
+    coords = await getCordsFromPostcode(postcode)
+    
+    users.insertMany({
+        firstname: firstname,
+        lastname: lastname,
+        donor: donor,
+        location: {type: "Point", coordinates: [coords.longitude, coords.latitude]},
+        bloodtype: bloodtype,
+        email: email,
+        password: password,
+        phonenumber: phone,
+        dateOfBirth: new Date(birthday)
+    }).then(console.log(`Inserted new user ${firstname}`))
+            
+    res.redirect("/")
     return
+
+    } catch(error){
+        console.log(error)
+    }
+
+    
 })
 
 function checkSession(req, res, next){
@@ -25,6 +50,26 @@ function checkSession(req, res, next){
         res.redirect("/")
     } else {
         next()
+    }
+}
+
+async function getCordsFromPostcode(postcode){
+    try{
+        const url = `https://api.postcodes.io/postcodes/${encodeURIComponent(postcode)}`
+        const response = await axios.get(url)
+        //console.log(response)
+        const data = response.data
+
+        if (data.status == 200){
+            const {longitude, latitude} = data.result
+
+            return {longitude: longitude, latitude: latitude}
+
+        }else{
+            console.log("an error occured with the api")
+        }
+    } catch(error){
+        console.log(error)
     }
 }
 
