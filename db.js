678d7d5c9cfdec1beae0d8d465f37d68dec30340
Ethan Weygang <ethanweygang@gmail.com>
2025-03-06 18:33:38 +0000

    Changed api key

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose")
 require("dotenv").config()  // Import and configure dotenv
+const apikey = process.env.MONGO_URI
 
 const connectDB = async () => {
     try {
         mongoose.set("strictQuery", false)
-        const conn = await mongoose.connect(process.env.MONGO_URI)  // Use the environment variable
+        const conn = await mongoose.connect(apikey)  // Use the environment variable
         console.log(`Database connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
